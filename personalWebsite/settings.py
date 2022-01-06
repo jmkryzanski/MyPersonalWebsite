76a489a9ad7e9cbdc6eba15371a0567a608ac04d
jmkryzanski <jmkryzanski@gmail.com>
2022-01-05 22:10:30 -0500

    new

--- personalWebsite/settings.py
+++ personalWebsite/settings.py
@@ -129,8 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#STATICFILES_DIRS = ['static']
-STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = ['static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
